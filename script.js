fe95ec069a22868f69812c20592969f64c219b0a
Miran <e227726@sbl.ch>
2022-11-16 14:39:12 +0100

    Remaking it

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 function handleKeyPress (ev) {
-    const player = document.querySelector("player")
+    const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
     player.setAttribute("cy", 100 * Math.random())
 }
