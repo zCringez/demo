24f4826b87d4a1c0fb42338e67bb489dbd55fcc9
Miran <e227726@sbl.ch>
2022-11-16 14:34:51 +0100

    RandomJumping

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 function handleKeyPress (ev) {
-    console.log (ev)
+    const player = document.querySelector("player")
+    player.setAttribute("cx", 100 * Math.random())
+    player.setAttribute("cy", 100 * Math.random())
 }
+
 
 window.onkeydown = handleKeyPress
