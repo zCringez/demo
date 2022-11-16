0529a94eeaeefe2339e85c52039c1b02061ca972
Miran <e227726@sbl.ch>
2022-11-16 14:47:31 +0100

    Changing Color green and red

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 
 function handleKeyPress (ev) {
+
+    if (ev.code === "Space"){
     const player = document.querySelector("#player")
     player.setAttribute("cx", 100 * Math.random())
-    player.setAttribute("cy", 100 * Math.random())
+    player.setAttribute("cy", 100 * Math.random())}
+else if (ev.key === "r"){
+
+player.setAttribute("fill", "red")
 }
+
+else if (ev.key === "g"){
+
+player.setAttribute("fill", "green")
+}
+
+
+
+
+}
+
+
 
 
 window.onkeydown = handleKeyPress
